8e04bcd41b6acb2e8255e1083e0f1318334a4b83
Evelynn Ghale <eveghale@Eve-MacBook-Pro.mshome.net>
2023-05-17 14:52:21 +0545

    Google Map Example Codes

---
+++ app/src/main/java/com/example/mygooglemap/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mygooglemap;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng pkcampus = new LatLng(27.7061029, 85.3181844);
+        mMap.addMarker(new MarkerOptions().position(pkcampus).title("Marker in PK Campus"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pkcampus));
+    }
+}
